472610f3862f09144d4938b4d87fe745f1eff9b4
raupsss <androghanz@gmail.com>
2022-12-30 15:14:01 +0700

    assignment 4

--- lib/basic_layout.dart
+++ lib/basic_layout.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 
@@ -34,20 +34,59 @@ class _BusinessCardState extends State<BusinessCard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            child: CircleAvatar(
-              backgroundImage: AssetImage("assets/images/myFace.jpg"),
+      body: Container(
+        color: Color.fromARGB(255, 240, 238, 238),
+        width: 330,
+        height: 150,
+        margin: EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/myFace.jpg"),
+                  ),
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Mr. Noerwachid",
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                    Text("Professional Player of Mobile Legends"),
+                  ],
+                ),
+              ],
             ),
-            color: Colors.amber,
-            padding: EdgeInsets.all(20.0),
-            margin: EdgeInsets.all(20.0),
-            alignment: Alignment.center,
-            width: 300,
-            height: 100,
-          )
-        ],
+            SizedBox(
+              height: 8,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Jl. Curug"),
+                Text("(415) 555-0198"),
+              ],
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(Icons.accessibility),
+                Icon(Icons.gamepad),
+                Icon(Icons.monetization_on_outlined),
+                Icon(Icons.phone_iphone),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
